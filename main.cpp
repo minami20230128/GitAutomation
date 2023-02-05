4c8ad70e179ce0da0404642a802f879e31e43024
minami20230128 <south614fruit@yahoo.co.jp>
2023-02-06 02:13:04 +0900

    add途中

--- main.cpp
+++ main.cpp
@@ -14,24 +14,33 @@ int main ()
         return EXIT_FAILURE;
     }
     
-    Pipe pipe;
-    pipe.open(command.c_str());
-    pipe.getResult();
-    pipe.close();
+    auto pipe = std::make_unique<Pipe>();
+    pipe->open(command.c_str());
+    pipe->getResult();
+    pipe->close();
 
     return 0;
 }
 
 void add()
 {
-    setDirectry();
+    auto filename = setDirectry();
+    auto pipe = std::make_unique<Pipe>();
+    std::string command = "add {} main", filename;
+    pipe->open(command.c_str());
+    pipe->getResult();
+    pipe->close();
+
 }
 
-void setDirectry(Pipe pipe)
+std::string setDirectry(Pipe pipe)
 {
+    std::string filename;
+    auto pipe = std::make_unique<Pipe>();
     char* command = "open /Desktop";
-    pipe.open(command);
-    pipe.close();
+    pipe->open(command);
+    pipe->close();
+    return filename;
 }
 
 void executeCommand()
