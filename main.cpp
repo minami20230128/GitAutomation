6c111b577bcc00da42179a05df37d01e96664111
minami20230128 <south614fruit@yahoo.co.jp>
2023-02-14 01:02:57 +0900

    add, commit機能作成

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
+#include <unistd.h>
+#include <vector>
+#include <boost/algorithm/string.hpp>
 #include "main.h"
 #include "pipe.cpp"
 
@@ -25,6 +28,8 @@ int main ()
     case 2:
         result = commit();
         break;
+    case 3:
+        result = push();
     }
 
     std::cout << result << std::endl;
@@ -37,7 +42,6 @@ std::string add()
     std::string filename;
     std::cout << "addしたいファイル名を入力してください" << std::endl;
     std::cin >> filename;
-    auto pipe = std::make_unique<Pipe>();
     std::string command = "git add " + filename + "  main";
     std::string result = executeCommand(command.c_str());
 
@@ -46,12 +50,33 @@ std::string add()
 
 std::string commit()
 {
-    auto pipe = std::make_unique<Pipe>();
-    std::string command = "git remote";
+    std::string message;
+    std::cout << "メッセージを入力してください" << std::endl;
+    std::cin >> message;
+    std::string command = "git commit -m " + message;
     std::string result = executeCommand(command.c_str());
 
+    return result;
+}
+
+std::string push()
+{
+    std::string command = "git remote";
+    std::string repositry = executeCommand(command.c_str());
     command = "git branch";
-    result = executeCommand(command.c_str());
+    std::string result = executeCommand(command.c_str());
+
+    std::vector<std::string> branches;
+    boost::split(branches, result, boost::is_any_of("¥n"));
+
+    for(auto branch: branches)
+    {
+        if(branch.find("*")!=-1)
+        {
+            command = "git push " + repositry + branch;
+            result = executeCommand(command.c_str());
+        }
+    }
 
     return result;
 }
@@ -59,9 +84,12 @@ std::string commit()
 std::string setDirectry(std::string directry)
 {
     std::string filename;
-    auto pipe = std::make_unique<Pipe>();
-    std::string command = "cd " + directry;
-    std::string result = executeCommand(command.c_str());
+    if (chdir(directry.c_str()) == -1){
+        return "such directry does not exist";
+    }
+
+    std::string command ="ls";//テストコード
+    std::string result = executeCommand(command.c_str());//テストコード
 
     return result;
 }
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
   
 std::string add();
 std::string commit();
+std::string push();
 std::string setDirectry(std::string directry);
 std::string executeCommand(std::string command);
 
